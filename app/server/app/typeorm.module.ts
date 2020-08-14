2aac58ae2228d9e6ee574a2af22643249f2c9e86
Michael <michael.baertschi@zebbra.ch>
2020-08-14 16:55:17 +0200

    Log database config

--- app/server/app/typeorm.module.ts
+++ app/server/app/typeorm.module.ts
@@ -3,6 +3,8 @@ import { ConfigService } from "@nestjs/config";
 
 export const typeormModule = TypeOrmModule.forRootAsync({
   inject: [ConfigService],
-  useFactory: (configService: ConfigService) =>
-    configService.get<TypeOrmModuleOptions>("database")!,
+  useFactory: (configService: ConfigService) => {
+    console.log(configService.get<TypeOrmModuleOptions>("database"));
+    return configService.get<TypeOrmModuleOptions>("database")!;
+  },
 });

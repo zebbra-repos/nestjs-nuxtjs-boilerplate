9d969ed6657edce07cd5346b79c11bb22360de03
Michael <michael.baertschi@zebbra.ch>
2020-08-26 09:48:26 +0200

    Fix graphql autoSchemaFile path on production

--- app/server/core/graphql.module.ts
+++ app/server/core/graphql.module.ts
@@ -4,11 +4,24 @@ import { ConfigService } from "@nestjs/config";
 export const graphQLModule = GraphQLModule.forRootAsync({
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => {
+    let autoSchemaFile;
+
+    switch (process.env.NODE_ENV) {
+      case "test":
+        autoSchemaFile = `${process.cwd()}/app/server/schema.gql`;
+        break;
+      case "development":
+        autoSchemaFile = true;
+        break;
+      case "production":
+        autoSchemaFile = `${process.cwd()}/dist/app/server/schema.gql`;
+        break;
+      default:
+        break;
+    }
+
     return {
-      autoSchemaFile:
-        process.env.NODE_ENV === "test"
-          ? true
-          : `${process.cwd()}/app/server/schema.gql`,
+      autoSchemaFile,
       installSubscriptionHandlers: true,
       debug: true,
       playground: configService.get<boolean>("production") === false,

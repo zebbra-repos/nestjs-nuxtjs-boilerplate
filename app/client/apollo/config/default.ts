7a1f2df02887d10f1ca62c9f370449ff6975c513
Michael <michael.baertschi@zebbra.ch>
2020-08-28 14:17:13 +0200

    Fix graphql endpoints for produciton build

--- app/client/apollo/config/default.ts
+++ app/client/apollo/config/default.ts
@@ -1,13 +1,12 @@
 import { Context } from "@nuxt/types";
 import errorLink from "~/apollo/links/error";
 
-export default function ({ isStatic, isClient, redirect }: Context) {
+export default function ({ redirect }: Context) {
   const httpEndpoint =
-    isStatic && isClient
-      ? window.location.protocol + "//" + window.location.host + "/graphql"
-      : "http://localhost:3000/graphql";
+    process.env.HTTP_GRAPHQL_ENDPOINT || "http://localhost:3000/graphql";
 
-  const wsEndpoint = httpEndpoint.replace(/^https?/, "ws");
+  const wsEndpoint =
+    process.env.WS_GRAPHQL_ENDPOINT || "ws://localhost:3000/graphql";
 
   return {
     link: errorLink(redirect),

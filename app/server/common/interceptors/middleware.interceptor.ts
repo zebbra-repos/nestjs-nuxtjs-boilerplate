01996e99c82bf975e04c5598d81049041f4a0a4e
Michael <michael.baertschi@zebbra.ch>
2020-09-09 15:55:26 +0200

    Set csrf token only for GET requests

--- app/server/common/interceptors/middleware.interceptor.ts
+++ app/server/common/interceptors/middleware.interceptor.ts
@@ -8,6 +8,7 @@ import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { GqlExecutionContext } from "@nestjs/graphql";
 import { ConfigService } from "@nestjs/config";
+import { Request } from "express";
 
 @Injectable()
 export class MiddlewareInterceptor implements NestInterceptor {
@@ -17,16 +18,19 @@ export class MiddlewareInterceptor implements NestInterceptor {
     return next.handle().pipe(
       map((data) => {
         const ctx = GqlExecutionContext.create(context);
-        const req = ctx.getContext().req;
+        const req: Request = ctx.getContext().req;
 
-        if (req.session) {
+        if (req.method === "GET" && req.session) {
           req.session.token = req.csrfToken();
         }
 
-        req.res.header(
-          "Access-Control-Allow-Origin",
-          this.configService.get<string>("accessControlAllowOrigin"),
-        );
+        if (req.res) {
+          req.res.header(
+            "Access-Control-Allow-Origin",
+            this.configService.get<string>("accessControlAllowOrigin"),
+          );
+        }
+
         return data;
       }),
     );

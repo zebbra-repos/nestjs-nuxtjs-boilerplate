e315d010cac6864c10e1e529c85790c11ed66f65
Michael <michael.baertschi@zebbra.ch>
2020-10-23 17:33:10 +0200

    Improve loading placeholder component

--- app/client/components/MyLoadingPlaceholder.vue
+++ app/client/components/MyLoadingPlaceholder.vue
@@ -1,5 +1,26 @@
 <template lang="pug">
-  v-card.pt-3(loading)
-    v-toolbar(color='accent' flat)
-      v-toolbar-title {{ $t('shared.loading') }}
+  span
 </template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  onBeforeMount,
+  onBeforeUnmount,
+} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  name: "MyLoadingPlaceholder",
+  setup() {
+    onBeforeMount(() => {
+      window.$nuxt.$nextTick(() => {
+        window.$nuxt.$loading.start();
+      });
+    });
+
+    onBeforeUnmount(() => {
+      setTimeout(() => window.$nuxt.$loading.finish(), 500);
+    });
+  },
+});
+</script>

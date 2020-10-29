70354c94dbbf984015c20dad8614301c691643ad
Michael <michael.baertschi@zebbra.ch>
2020-10-29 10:30:02 +0100

    Fix i18n on client

--- app/client/locales/de.js
+++ app/client/locales/de.js
@@ -1,3 +1,8 @@
 import { loadTranslations } from "~/utils/i18n";
 
-export default () => loadTranslations("de", "app/client/locales");
+const path =
+  process.env.NODE_ENV === "production"
+    ? "dist/app/client/dist/locales"
+    : "app/client/locales";
+
+export default () => loadTranslations("de", path);
